20b62a255cb5906cc3d7c590430e87ffb3206e84
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 13:20:51 +0900

    Initial Commit

---
+++ Jyanken/JyankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JyankenApp.swift
+//  Jyanken
+//
+//  Created by 飯塚政美 on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct JyankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
